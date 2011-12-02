a1ed8ea7dc65298219eec082b4bf48629a99a35f
ngadiono <cristiano_ondoel@yahoo.com>
2011-12-02 14:16:43 +0700

    benerin bgsamping

--- application/views/user/frontend/scroll/v_scroll.php
+++ application/views/user/frontend/scroll/v_scroll.php
@@ -1,9 +1,17 @@
 <div id="scrollbar1">
-
+    <style>
+                .overview span
+                {
+                    color:  white;
+                    position: static;
+                    margin-right: -317px
+                }
+            </style>
     <div class="scrollbar"><div class="track"><div class="thumb"><div class="end"></div></div></div></div>
     <div class="viewport">
         <div class="overview">
-
+            
+            
             
             <h3>Sketsa</h3>
 
@@ -16,67 +24,51 @@
                 oleh MENAKAR FILSAFAT DI PO2 kebijaksanaan (love of wisdom) Dan kata filsafat merupakan Arabisasi, suatu mashdar yang berarti kerja atau pencarian yang 
             </p>
             
-            <ul style="margin: 0; padding: 0;">
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-                <li style="display: inline-block; margin-left: 5px; margin-right: 5px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-            </ul>
-            <h3>Magnis dis parturient montes</h3>
-            <hr>
-            <h4>judul</h4>
-            
-            <p><img class="img_yayasan" src="<?php echo base_url(); ?>image/Barcelona_128.png"/>ondoel ondoel ondeol ondoel ondoel ondoels iaculis eu. Duis egestas augue quis ante ornare eu tincidunt magna interdum. Vestibulum posuere risus non ipsum sollicitudin quis viverra ante feugiat. Pellentesque non faucibus lorem. Nunc tincidunt diam nec risus ornare quis porttitor enim pretium. Ut adipiscing tempor massa, a ullamcorper massa bibendum at. Suspendisse potenti. In vestibulum enim orci, nec consequat turpis. Suspendisse sit amet tellus a quam volutpat porta. Mauris ornare augue ut diam tincidunt elementum. Vivamus commodo dapibus est, a gravida lorem pharetra eu. Maecenas ultrices cursus tellus sed congue. Cras nec nulla erat.</p>
+            <span>baca selengkapnya</span>
             
             <ul style="margin: 0; padding: 0;">
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-                <li style="display: inline-block; margin-left: 5px; margin-right: 5px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-            </ul>
-            <h3>Magnis dis parturient montes</h3>
-            <hr>
-            <h4>judul</h4>
-            
-            <p><img class="img_yayasan" src="<?php echo base_url(); ?>image/Barcelona_128.png"/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae velit at velit pretium sodales. Maecenas egestas imperdiet mauris, vel elementum turpis iaculis eu. Duis egestas augue quis ante ornare eu tincidunt magna interdum. Vestibulum posuere risus non ipsum sollicitudin quis viverra ante feugiat. Pellentesque non faucibus lorem. Nunc tincidunt diam nec risus ornare quis porttitor enim pretium. Ut adipiscing tempor massa, a ullamcorper massa bibendum at. Suspendisse potenti. In vestibulum enim orci, nec consequat turpis. Suspendisse sit amet tellus a quam volutpat porta. Mauris ornare augue ut diam tincidunt elementum. Vivamus commodo dapibus est, a gravida lorem pharetra eu. Maecenas ultrices cursus tellus sed congue. Cras nec nulla erat.</p>
-            
-            <ul style="margin: 0; padding: 0;">
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-                <li style="display: inline-block; margin-left: 5px; margin-right: 5px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
+                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px; margin-left: -20px;"></li>
+                <li style="display: inline-block; margin-left: 10px; margin-right: 10px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
                 <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
             </ul>
-            <h3>Magnis dis parturient montes</h3>
-            <hr>
-            <h4>judul</h4>
             
-            <p><img class="img_yayasan" src="<?php echo base_url(); ?>image/Barcelona_128.png"/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae velit at velit pretium sodales. Maecenas egestas imperdiet mauris, vel elementum turpis iaculis eu. Duis egestas augue quis ante ornare eu tincidunt magna interdum. Vestibulum posuere risus non ipsum sollicitudin quis viverra ante feugiat. Pellentesque non faucibus lorem. Nunc tincidunt diam nec risus ornare quis porttitor enim pretium. Ut adipiscing tempor massa, a ullamcorper massa bibendum at. Suspendisse potenti. In vestibulum enim orci, nec consequat turpis. Suspendisse sit amet tellus a quam volutpat porta. Mauris ornare augue ut diam tincidunt elementum. Vivamus commodo dapibus est, a gravida lorem pharetra eu. Maecenas ultrices cursus tellus sed congue. Cras nec nulla erat.</p>
-            
-            <ul style="margin: 0; padding: 0;">
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-                <li style="display: inline-block; margin-left: 5px; margin-right: 5px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-            </ul>
-            <h3>Magnis dis parturient montes</h3>
-            <hr>
-            <h4>judul</h4>
-            
-            <p><img class="img_yayasan" src="<?php echo base_url(); ?>image/Barcelona_128.png"/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae velit at velit pretium sodales. Maecenas egestas imperdiet mauris, vel elementum turpis iaculis eu. Duis egestas augue quis ante ornare eu tincidunt magna interdum. Vestibulum posuere risus non ipsum sollicitudin quis viverra ante feugiat. Pellentesque non faucibus lorem. Nunc tincidunt diam nec risus ornare quis porttitor enim pretium. Ut adipiscing tempor massa, a ullamcorper massa bibendum at. Suspendisse potenti. In vestibulum enim orci, nec consequat turpis. Suspendisse sit amet tellus a quam volutpat porta. Mauris ornare augue ut diam tincidunt elementum. Vivamus commodo dapibus est, a gravida lorem pharetra eu. Maecenas ultrices cursus tellus sed congue. Cras nec nulla erat.</p>
-            
-            <ul style="margin: 0; padding: 0;">
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-                <li style="display: inline-block; margin-left: 5px; margin-right: 5px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-            </ul>
-            <h3>Magnis dis parturient montes</h3>
-            <hr>
-            <h4>judul</h4>
-            
-            <p><img class="img_yayasan" src="<?php echo base_url(); ?>image/Barcelona_128.png"/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae velit at velit pretium sodales. Maecenas egestas imperdiet mauris, vel elementum turpis iaculis eu. Duis egestas augue quis ante ornare eu tincidunt magna interdum. Vestibulum posuere risus non ipsum sollicitudin quis viverra ante feugiat. Pellentesque non faucibus lorem. Nunc tincidunt diam nec risus ornare quis porttitor enim pretium. Ut adipiscing tempor massa, a ullamcorper massa bibendum at. Suspendisse potenti. In vestibulum enim orci, nec consequat turpis. Suspendisse sit amet tellus a quam volutpat porta. Mauris ornare augue ut diam tincidunt elementum. Vivamus commodo dapibus est, a gravida lorem pharetra eu. Maecenas ultrices cursus tellus sed congue. Cras nec nulla erat.</p>
-            
-            <ul style="margin: 0; padding: 0;">
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-                <li style="display: inline-block; margin-left: 5px; margin-right: 5px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
-                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
-            </ul>
+            <h3>Sketsa</h3>
 
+            <hr>
+            <h4>MENAKAR FILSAFAT DI PANGGUNG REALITAS</h4>
+            
+            <p>
+                <img class="img_yayasan" src="<?php echo base_url(); ?>image/Barcelona_128.png"/>terminologi filsafat mempunyai kompleksitas pemikiran yang beragam karena mempunyai pembacaan terhadap metode, aliran dan tujuan yang merupakan persentasi dalam seluruh konstruk pemikiran 
+                seorang filsuf. disisi lain, filsafat bukan berasal dari ketiadaan serta perlu memperhatikan latar belakang kerja atau pencarian , lous kattsoff dalam. yang dilakukan 
+                oleh MENAKAR FILSAFAT DI PO2 kebijaksanaan (love of wisdom) Dan kata filsafat merupakan Arabisasi, suatu mashdar yang berarti kerja atau pencarian yang 
+            </p>
+            
+            <span>baca selengkapnya</span>
+            
+            <ul style="margin: 0; padding: 0;">
+                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px; margin-left: -20px;"></li>
+                <li style="display: inline-block; margin-left: 10px; margin-right: 10px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
+                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
+            </ul>
+            
+            <h3>Sketsa</h3>
+
+            <hr>
+            <h4>MENAKAR FILSAFAT DI PANGGUNG REALITAS</h4>
+            
+            <p>
+                <img class="img_yayasan" src="<?php echo base_url(); ?>image/Barcelona_128.png"/>terminologi filsafat mempunyai kompleksitas pemikiran yang beragam karena mempunyai pembacaan terhadap metode, aliran dan tujuan yang merupakan persentasi dalam seluruh konstruk pemikiran 
+                seorang filsuf. disisi lain, filsafat bukan berasal dari ketiadaan serta perlu memperhatikan latar belakang kerja atau pencarian , lous kattsoff dalam. yang dilakukan 
+                oleh MENAKAR FILSAFAT DI PO2 kebijaksanaan (love of wisdom) Dan kata filsafat merupakan Arabisasi, suatu mashdar yang berarti kerja atau pencarian yang 
+            </p>
+            
+            <span>baca selengkapnya</span>
+            
+            <ul style="margin: 0; padding: 0;">
+                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px; margin-left: -20px;"></li>
+                <li style="display: inline-block; margin-left: 10px; margin-right: 10px"><img src="<?php echo base_url(); ?>image/scrolIco.png" style="height: 30px; width: 30px"/></li>
+                <li style="display: inline-block"><hr style="border: 1px solid rgb(0,102,51); width: 210px;"></li>
+            </ul>
 
         </div>
     </div>
